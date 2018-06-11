9351a7c3fe07ce0e9124fd0985fe1a9cf662ca32
mrutyunjayya <mrutyunjayya.m@247-inc.com>
2018-06-11 12:04:56 +0530

    added shoppe agent request

--- server.js
+++ server.js
@@ -30,6 +30,47 @@ restService.post("/echo", function (req, res) {
 
 });
 
+restService.post("/shoppe", function (req, res) {
+    var speech = "";
+    //var speech = req.body.queryResult && req.body.queryResult.parameters && req.body.queryResult.parameters.orderstatus ? OrderStatus : "You don't have any active orders in your list";
+
+    if (req.body.queryResult.parameters.welcome) {
+        speech = "Hello, PV. Do you have a question about your recent order ?";
+        return res.json({
+            fulfillmentText: speech,
+            fulfillmentMessages: [{
+                text: {
+                    text: [speech]
+                }
+            }],
+            source: "basic-nodejs"
+        });
+    } else if (req.body.queryResult.parameters.orderstatus) {
+        speech = "see that your Nest is out for delivery, so it should arrive today.";
+        return res.json({
+            fulfillmentText: speech,
+            fulfillmentMessages: [{
+                text: {
+                    text: [speech]
+                }
+            }],
+            source: "basic-nodejs"
+        });
+    } else {
+        speech = "Sorry You don't have any active orders in your list";
+        return res.json({
+            fulfillmentText: speech,
+            fulfillmentMessages: [{
+                text: {
+                    text: [speech]
+                }
+            }],
+            source: "basic-nodejs"
+        });
+
+    }
+});
+
 restService.listen(process.env.PORT || 8000, function () {
     console.log("Server up and listening");
 });

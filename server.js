0334fa8e8407fe8749420436d3b0a3aadd258d5d
mrutyunjayya <mrutyunjayya.m@247-inc.com>
2018-06-30 22:59:38 +0530

    test

--- server.js
+++ server.js
@@ -37,36 +37,31 @@ restService.post("/shoppe", function (req, res) {
     //var speech = req.body.queryResult && req.body.queryResult.parameters && req.body.queryResult.parameters.orderstatus ? OrderStatus : "You don't have any active orders in your list";
 
     if (req.body.queryResult.action == 'input.welcome') {
-        //speech = "Hello, PV. Do you have a question about your recent order ?";
+        speech = "Hello, PV. Do you have a question about your recent order ?";
 
-        let option = {
-            hostname:'cloudcontactcenter.googleapis.com/v1alpha1',
-            port: 443,
-            path: '/projects/contact-center-206218/locations/mj_shoppe/knowledgeBases',
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json; charset=utf-8',
-                'Authorization': 'Bearer ya29.c.El_pBWrPw6OqOIPELAS-XJlnQH78Gwccpj1g1wNVUp-IM-uKexN4Cp1hOGhPuxLZGERZAGqA1SbmtKzl3jCNGGlY7zd8ZG0Yzkl1Zk9viwzN95b4JsPQhPkoSrMykYiNLA'
-            }
-        }
+        
 
         var request = https.request(options, (res) => {
             let speechOutput = '';
             res.setEncoding('utf8')
-            res.on('data', (chunk) => {
-                console.log(`Got partial data ${chunk}`)
-                data += chunk;
-
-            })
+          
             res.on('end', () => {
                 console.log(`done handling request data = ${data}`)
 
-                let ret = JSON.parse(data);
+                let ret = JSON.parse(res);
+                console.log(res);
 
                 speech = ret;
 
             })
         })
+
+        request.on('error', (e) => {
+            console.log(`**************** problem with request: ${e.message}`);
+        });
+
+        
+        request.end()
 
 
 
